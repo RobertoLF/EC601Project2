c83a79ca44009bb1432322050baac1b154c53bae
RobertoLF <rluisfue@broadinstitute.org>
2021-11-06 17:41:12 -0400

    Revising productV2 and adding tests for pytest module.

--- EC601Project2ProductV2.py
+++ EC601Project2ProductV2.py
@@ -9,6 +9,15 @@ import base64
 from google.cloud import language_v1
 import os 
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/rluisfue/Documents/ec601-327314-c0d53011a952.json"
+client = language_v1.LanguageServiceClient()
+start = ""
+end = ""
+queryStart = ""
+queryEnd = ""
+TimeError = 0
+errors = {"":""}
+
 def getBearerToken():
     bearerURL = "https://api.twitter.com/oauth2/token"
     parameter = {'grant_type':"client_credentials"}
@@ -26,7 +35,7 @@ def getBearerToken():
     inspect=r.json()
     return inspect["access_token"]
 
-def getTweets(token,userId,nextToken,getDate):
+def getTweets(token,userId,nextToken,getDate,testMode):
     mentionsURL = "https://api.twitter.com/2/users/" + userId + "/tweets"
     if getDate == 1:
         global start
@@ -49,14 +58,17 @@ from which tweets will be provided in the format 'YYYY-MM-DD':""")
         queryStart = start + "T00:00:01-04:00"
         global queryEnd
         queryEnd = end + "T00:00:01-04:00"
-    parameters = {'max_results':100,'start_time':queryStart,'end_time':queryEnd}
+    if testMode == True:
+        parameters = {'max_results':100}
+    else:
+        parameters = {'max_results':100,'start_time':queryStart,'end_time':queryEnd}
     if nextToken != 0:
         parameters["pagination_token"] = nextToken
     headers = {'Authorization':"Bearer " + token}
     r = requests.get(url=mentionsURL,params=parameters,headers=headers)
     inspect = r.json() 
     global errors
-    global params
+    params = {}
     if "errors" in inspect:
         errorList = inspect["errors"]
         errors = errorList[0]
@@ -82,89 +94,87 @@ def getUserID(handle,token):
         userID = userData["data"]["id"]
     return userID
 
-def getSentiment(tweets):
-    textToAnalyze = str(tweets)
+def getSentiment(textToAnalyze):
     googleDocument = language_v1.Document(content=textToAnalyze, type_=language_v1.Document.Type.PLAIN_TEXT)
     sentiment = client.analyze_sentiment(request={'document':googleDocument}).document_sentiment
     score = sentiment.score
+    translation = ""
     if score < -0.75 :
-        print("The sentiment of the found tweets is nearly entirely negative! :( \n")
+        translation = "The sentiment of the found tweets is nearly entirely negative! :( \n"
     elif score < -0.5 and score > -0.75 :
-        print("The sentiment of the found tweets is very negative. \n ")
+        translation = "The sentiment of the found tweets is very negative. \n "
     elif score < -0.25 and score > -0.5 :
-        print("The sentiment of the found tweets is negative. \n")
+        translation = "The sentiment of the found tweets is negative. \n"
     elif score < 0 and score > -0.25 : 
-        print("The sentiment of the found tweets is slightly negative and nearly neutral. \n")
+        translation = "The sentiment of the found tweets is slightly negative and nearly neutral. \n"
     elif score == 0 :
-        print("The sentiment of the found tweets is neutral. :| \n")
+        translation = "The sentiment of the found tweets is neutral. :| \n"
     elif score > 0 and score < 0.25:
-        print("The sentiment of the found tweets is slightly positive and nearly neutral. \n")
+        translation = "The sentiment of the found tweets is slightly positive and nearly neutral. \n"
     elif score > 0.25 and score < 0.5 :
-        print("The sentiment of the found tweets is negative. \n")
+        translation = "The sentiment of the found tweets is negative. \n"
     elif score > 0.5 and score < 0.75 :
-        print("The sentiment of the found tweets is very positive. \n")
+        translation = "The sentiment of the found tweets is very positive. \n"
     elif score > 0.75:
-        print("The sentiment of the found tweets is nearly entirely positive! :) \n")
+        translation = "The sentiment of the found tweets is nearly entirely positive! :) \n"
+    return translation
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/rluisfue/Documents/ec601-327314-c0d53011a952.json"
-client = language_v1.LanguageServiceClient()
-interrupt = 'Y'
-firstime =  1
-start = ""
-end = ""
-queryStart = ""
-queryEnd = ""
-TimeError = 0
-errors = {"":""}
-params = {"":""}
-saveResults = "N"
-
-while (interrupt == 'Y'):
-    if firstime == 1:
-        print("Hello!\n")
-        print("""This program searches twitter for all the tweets of a specified user
+def main():
+    interrupt = 'Y'
+    firstime =  1
+    global start
+    global end
+    global TimeError
+    global errors
+    while (interrupt == 'Y'):
+        if firstime == 1:
+            print("Hello!\n")
+            print("""This program searches twitter for all the tweets of a specified user
 given a start/end date and subsequently determines the sentiment of the found tweets.
-The maximum number of Tweets this program can find is 700. \n""")
-        
-    handle = input("Enter a twitter handle to search tweets for: ")
-    print("\n")
-    token = getBearerToken()
-    userID = getUserID(handle,token)
-    nextToken = 0
-    tweetCount= 0
-    getDate = 1
-    skip = 0
-    tweets  = {}
-    while nextToken != 1 and userID != "" and TimeError == 0:
-        tweets.update(getTweets(token,userID,nextToken,getDate))
-        if TimeError == 0:
-            if "meta" in tweets:
-                meta = tweets["meta"]
-            if "data" in tweets:  
-                tweetCount += len(tweets["data"])
-            if "next_token" in meta:
-                nextToken = meta["next_token"]
-            else:
-                nextToken =1
-            getDate = 0
-        if tweetCount > 800:
-            print("""Found  %d tweets for the %s handle between the dates of %s & %s.
+The maximum number of Tweets this program can find is 3200. \n""")
+            
+        handle = input("Enter a twitter handle to search tweets for: ")
+        print("\n")
+        token = getBearerToken()
+        userID = getUserID(handle,token)
+        nextToken = 0
+        tweetCount= 0
+        getDate = 1
+        skip = 0
+        tweets  = {}
+        while nextToken != 1 and userID != "" and TimeError == 0:
+            tweets.update(getTweets(token,userID,nextToken,getDate,False))
+            if TimeError == 0:
+                if "meta" in tweets:
+                    meta = tweets["meta"]
+                if "data" in tweets:  
+                    tweetCount += len(tweets["data"])
+                if "next_token" in meta:
+                    nextToken = meta["next_token"]
+                else:
+                    nextToken =1
+                getDate = 0
+            if tweetCount > 3200:
+                print("""Found  %d tweets for the %s handle between the dates of %s & %s.
 That is the limit of tweets this program can access! \n""" %(tweetCount,handle,start,end))
-            skip = 1
-             
-    if userID != "" and TimeError == 0 and skip == 0:    
-        print("""
-Found %d tweets for the %s handle between the dates of %s & %s. \n""" %(tweetCount,handle,start,end))     
-
-    if tweetCount > 0:
-        getSentiment(tweets)
+                skip = 1
+                 
+        if userID != "" and TimeError == 0 and skip == 0:    
+            print("Found %d tweets for the %s handle between the dates of %s & %s. \n" %(tweetCount,handle,start,end))     
+    
+        if tweetCount > 0:
+            textToAnalyze = ""
+            for x in tweets['data']:
+                textToAnalyze = textToAnalyze + x['text'] + "\n"
+            sentimentAnalysis = getSentiment(textToAnalyze)
+            print(sentimentAnalysis)
+            
+        interrupt = input("Would you like to try again? (Y/N): ")
+        print('\n')
+        firstime = 0
+        TimeError = 0
+        errors = {}
+        tweets = {}
         
-    interrupt = input("Would you like to try again? (Y/N): ")
-    print('\n')
-    firstime = 0
-    TimeError = 0
-    errors = {}
-    params = {}
-    tweets = {}
-    queryStart = ""
-    queryEnd = ""
+if __name__ == "__main__":
+    main()
